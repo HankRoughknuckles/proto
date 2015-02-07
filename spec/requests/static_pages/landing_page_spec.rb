c706847e129e467d10351898b5d09e009dfa271f
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-02-07 17:52:31 +0200

    write pending specs

--- spec/requests/static_pages/landing_page_spec.rb
+++ spec/requests/static_pages/landing_page_spec.rb
@@ -22,5 +22,17 @@ describe "The landing page" do
     it 'should have proto for a title' do
       expect(ui).to have_proper_title
     end
+
+    it 'should have a link to the login page'
+  end
+
+  describe "the signup form" do
+    before { ui.visit_page_as nil }
+
+    it 'should go to the Ideas index upon logging in'
+
+    it 'should flash an error if the email is missing'
+
+    it 'should flash an error if the password is missing'
   end
 end
