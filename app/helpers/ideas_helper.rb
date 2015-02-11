64753e473f91065815f6745745f5a5fe193931f7
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-02-11 19:23:51 +0200

    upvote_link_for helper gives link to sign in if not logged in

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,12 +1,18 @@
 module IdeasHelper
   def upvote_link_for(idea)
-    link_to 'Upvote', upvote_idea_path(idea), 
-      {
+    if user_signed_in?
+      return link_to 'Upvote', upvote_idea_path(idea), {
         method: :put, 
         class: "upvote upvote-#{idea.id}",
         title: "Click if you like this idea",
         remote: true
       }
+    else
+      return link_to "Upvote", new_user_session_path, {
+        class: "upvote upvote-#{idea.id}", 
+        title: "You must be logged in to upvote"
+      }
+    end
   end
 
   def downvote_link_for(idea)
