76355bfbece60f04db35e3484fb88adbdd5f58d2
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-02-13 11:33:55 +0200

    serve static assets

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Proto
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #make assets work in heroku
+    config.serve_static_assets = true
   end
 end
