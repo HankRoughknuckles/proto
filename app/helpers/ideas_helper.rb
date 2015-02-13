9015c043a1949f5ba3522dd5f61f5ebf68d48bbb
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-02-13 11:12:07 +0200

    extract login_link_attrs method

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -14,13 +14,12 @@ module IdeasHelper
   # displays the link for upvoting for the passed idea
   def upvote_link_for(idea)
     if user_signed_in?
-      return link_to 'Upvote', upvote_idea_path(idea), upvote_attrs(idea)
+      path = upvote_idea_path(idea)
+      return link_to 'Upvote', path, upvote_attrs(idea)
 
     else
-      return link_to "Upvote", new_user_session_path, {
-        class: "upvote upvote-#{idea.id}", 
-        title: "You must be logged in to upvote"
-      }
+      path = new_user_session_path
+      return link_to "Upvote", path, login_link_attrs(:upvote)
     end
   end
 
@@ -28,26 +27,12 @@ module IdeasHelper
   # displays the link for downvoting for the passed idea
   def downvote_link_for(idea)
     if user_signed_in?
-      class_attr = "downvote downvote-#{idea.id}"
-      vote = current_user.voted_as_when_voted_for idea
-      if (vote != nil) && (vote == false)
-        class_attr = class_attr + " selected" 
-      end
+      path = downvote_idea_path(idea)
+      return link_to 'Downvote', path, downvote_attrs(idea) 
 
-      return link_to 'Downvote', downvote_idea_path(idea), {
-        method:       :put, 
-        class:        class_attr,
-        title:        "Click if you think this is a bad idea",
-        remote:       true,
-        data: {
-          dbid: idea.id
-        }
-      }
     else
-      return link_to "Downvote", new_user_session_path, {
-        class: "downvote downvote-#{idea.id}", 
-        title: "You must be logged in to downvote"
-      }
+      path = new_user_session_path
+      return link_to "Downvote", path, login_link_attrs(:downvote)    
     end
   end
 
@@ -65,6 +50,37 @@ module IdeasHelper
       data: {
         dbid: idea.id
       }
+    }
+  end
+
+      
+  # returns a hash of html_attrs to be added to an downvote button
+  def downvote_attrs(idea)
+    class_attr = "downvote downvote-#{idea.id}"
+    class_attr = class_attr + " selected" if current_user.downvoted? idea
+
+    return {
+      method:       :put, 
+      class:        class_attr,
+      title:        "Click if you think this is a bad idea",
+      remote:       true,
+      data: {
+        dbid: idea.id
+      }
+    }
+  end
+
+
+  def login_link_attrs(votetype)
+    if votetype == :upvote
+      class_attr = "upvote upvote-#{idea.id}"
+    elsif votetype == :downvote
+      class_attr = "downvote downvote-#{idea.id}"
+    end
+
+    return {
+      class: class_attr, 
+      title: "You must be logged in to upvote"
     }
   end
 end
