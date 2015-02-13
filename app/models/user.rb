724ce0b0669603d7940b560576a2ed6e056755b7
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-02-13 11:00:32 +0200

    Make voted_for? upvoted? and downvoted? for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,23 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  def voted_for?(idea)
+    vote = self.voted_as_when_voted_for idea
+    return vote != nil
+  end
+
+
+  def upvoted?(idea)
+    vote = self.voted_as_when_voted_for idea
+
+    return self.voted_for?(idea) && (vote == true)
+  end
+
+
+  def downvoted?(idea)
+    vote = self.voted_as_when_voted_for idea
+
+    return self.voted_for?(idea) && (vote == false)
+  end
 end
