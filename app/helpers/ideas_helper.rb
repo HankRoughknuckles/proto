bb2d63dea4d0543b1a91b2ec909fcc179bf14ade
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-02-13 11:01:40 +0200

    make upvote_attrs method extraction

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -14,18 +14,8 @@ module IdeasHelper
   # displays the link for upvoting for the passed idea
   def upvote_link_for(idea)
     if user_signed_in?
-      class_attr = "upvote upvote-#{idea.id}"
-      class_attr = class_attr + " selected" if current_user.voted_as_when_voted_for idea
+      return link_to 'Upvote', upvote_idea_path(idea), upvote_attrs(idea)
 
-      return link_to 'Upvote', upvote_idea_path(idea), {
-        method:       :put, 
-        class:        class_attr,
-        title:        "Click if you like this idea",
-        remote:       true,
-        data: {
-          dbid: idea.id
-        }
-      }
     else
       return link_to "Upvote", new_user_session_path, {
         class: "upvote upvote-#{idea.id}", 
@@ -59,5 +49,22 @@ module IdeasHelper
         title: "You must be logged in to downvote"
       }
     end
+  end
+
+
+  # returns a hash of html_attrs to be added to an upvote button
+  def upvote_attrs(idea)
+    class_attr = "upvote upvote-#{idea.id}"
+    class_attr = class_attr + " selected" if current_user.upvoted? idea
+
+    return {
+      method:       :put, 
+      class:        class_attr,
+      title:        "Click if you like this idea",
+      remote:       true,
+      data: {
+        dbid: idea.id
+      }
+    }
   end
 end
