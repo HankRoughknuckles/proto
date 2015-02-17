656b46c81153324d5ec202bb71afd81c93597150
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-02-17 18:56:37 +0200

    Add category to idea factory

--- spec/factories/ideas.rb
+++ spec/factories/ideas.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :idea do
     sequence(:title)            { |n| "Great idea ##{n}" }
     description                 "This is the best thing ever, trust me"
+    category                    Idea::MISCELLANEOUS
     user
   end
 end
