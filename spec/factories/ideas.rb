05aede4d3563af416656bef51b152763b9ae2b59
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-02-18 18:56:48 +0200

    remove category from idea factory

--- spec/factories/ideas.rb
+++ spec/factories/ideas.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :idea do
     sequence(:title)            { |n| "Great idea ##{n}" }
     description                 "This is the best thing ever, trust me"
-    category                    Idea::MISCELLANEOUS
     user
   end
 end
