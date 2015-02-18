a53de3c4a4b96547663966273e8c69914de67155
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-02-18 18:56:16 +0200

    Create category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217164249) do
+ActiveRecord::Schema.define(version: 20150217191845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
@@ -26,7 +32,7 @@ ActiveRecord::Schema.define(version: 20150217164249) do
     t.string   "main_image_content_type"
     t.integer  "main_image_file_size"
     t.datetime "main_image_updated_at"
-    t.integer  "category"
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
